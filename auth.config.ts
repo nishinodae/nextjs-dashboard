e880a6525ab8d2e1d5990992d1e172b9866a0542
nishinodae <masyitahsalwa@gmail.com>
2025-11-24 23:03:52 +0800

    export authConfig object that contain configuration options for NextAuth.js

---
+++ auth.config.ts
@@ -0,0 +1,25 @@
+import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    //auth contains the user's session
+    //request contains incoming request
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  //providers list the different login options such as Google, GitHub, email, OAuth, Credentials(username and password)
+  providers: [Credentials({})],
+} satisfies NextAuthConfig;
