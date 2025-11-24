b6e1c77fcd39f99d7082e39fd5c41f29f5e6b993
nishinodae <masyitahsalwa@gmail.com>
2025-11-24 23:17:47 +0800

    import signIn function from auth.ts, and created a new action called authenticate. signIn is used in authenticate()

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -112,4 +114,23 @@ export async function deleteInvoice(id: string) {
     // }
     //revalidate the path, so cache is cleared and fresh data is fetched
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState:string | undefined,
+    formData: FormData
+) {
+    try{
+        await signIn('credentials', formData);
+    }catch(e){
+        if(e instanceof AuthError){
+            switch(e.type){
+                case 'CredentialsSignin':
+                    return 'Invalid credentials';
+                default:
+                    return 'Something went wrong';
+            }
+        }
+        throw e;
+    }
 }
