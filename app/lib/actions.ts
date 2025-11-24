0ab57b8de21a6d4848795498174401597fa39d48
nishinodae <masyitahsalwa@gmail.com>
2025-11-24 19:23:21 +0800

    server-side validation for EditInvoice form using useActionState, zod, safeParse

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,17 +10,17 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
-        invalid_type_error:'Please select a customer.'
+        invalid_type_error: 'Please select a customer.'
     }),
-    amount: z.coerce.number().gt(0,{message:'Please enter an amount greater than 0.'}),
-    status: z.enum(['pending', 'paid'],{invalid_type_error:'Please select an invoice status.'}),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than 0.' }),
+    status: z.enum(['pending', 'paid'], { invalid_type_error: 'Please select an invoice status.' }),
     date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export type State = {
-    errors?:{
+    errors?: {
         customerId?: string[],
         amount?: string[],
         status?: string[],
@@ -28,16 +28,16 @@ export type State = {
     message?: string | null;
 };
 
-export async function createInvoice(prevState:State, formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
     const rawFormData = {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     }
     const validatedFields = CreateInvoice.safeParse(rawFormData);
-    
+
     //if form validation fails, return errors early. Otherwise, continue.
-    if(!validatedFields.success){
+    if (!validatedFields.success) {
         return {
             errors: validatedFields.error.flatten().fieldErrors,
             message: 'Missing fields. Failed to Create Invoice.'
@@ -68,25 +68,32 @@ export async function createInvoice(prevState:State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     const rawFormData = {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     }
-    const { customerId, amount, status } = UpdateInvoice.parse(rawFormData);
+    const validatedFields = UpdateInvoice.safeParse(rawFormData);
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Failed to Create Invoice.'
+        }
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    // try {
-        //update invoices table with the validated data
-        await sql`
+    try {
+    //update invoices table with the validated data
+    await sql`
         UPDATE invoices 
         SET customer_id = ${customerId}, amount = ${amountInCents}, status =  ${status}, date = ${date}
         WHERE id = ${id}`;
-    // } catch (e) {
-    //     console.error(e);
-    //     return { message: 'Database Error: Failed to Update Invoice' }
-    // }
+    } catch (e) {
+        console.error(e);
+        return { message: 'Database Error: Failed to Update Invoice' }
+    }
 
     //revalidate the path, so cache is cleared and fresh data is fetched
     revalidatePath('/dashboard/invoices');
@@ -97,8 +104,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
     // try {
-        //delete invoice from the invoices table
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    //delete invoice from the invoices table
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
     // } catch (e) {
     //     console.error(e);
     //     return { message: 'Database Error: Failed to Delete Invoice' }
